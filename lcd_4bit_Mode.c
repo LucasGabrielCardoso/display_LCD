dcc31bb37c5d9fd07568a90373b139865c3d21c3
LucasGabrielCardoso <l.c-gabriel@hotmail.com>
2022-12-26 19:59:39 -0300

    driver lcd v0

---
+++ lcd_4bit_Mode.c
@@ -0,0 +1,263 @@
+/*
+ * File:   lcd.c
+ * Author: LucasGabriel
+ *
+ * Created on December 9, 2022, 9:06 PM
+ */
+
+#include "lcd_4bit_Mode.h"
+#include "system_config.h"
+#include "device_config.h"
+#include "delay.h"
+
+/******************************************************************************/
+/*                             Display Defines                                */
+/******************************************************************************/
+//Set DDRAM address
+#define SET_DDRAM_ADDRESS           0x80
+
+//Set CGRAM Address
+#define SET_CGRAM_ADDRESS           0x40
+
+//Function Set
+#define DATA_8_BIT_MODE             0x30
+#define TWO_LINE_DISPLAY            0x28
+#define FIVE_BY_TEN_DOTS_MATRIX     0x24
+#define DATA_4_BIT_MODE             0x20
+
+//Cursor or Display Shift
+#define DISPLAY_SHIFT               0x18
+#define SHIFT_TO_RIGHT              0x14
+
+//Display on/off
+#define DISPLAY_ON                  0x0C
+#define CURSOR_ON                   0x0A
+#define BLINKING_CURSOR_ON          0x09
+#define DISPLAY_OFF                 0x08
+
+//Entry mode set
+#define MOVE_FOWARD                 0x06
+#define SHIFT_ENTYRE_DISPLAY        0x05
+
+//Return home
+#define RETURN_HOME                 0x03 //This instruction takes a bit more time (~1.52 ms)
+                                         //therefore, always put a Delay_ms(2) after instruction
+//Clear Display
+#define CLEAR_DISPLAY               0x01 //This instruction takes a bit more time (~1.52 ms)
+                                         //therefore, always put a Delay_ms(2) after instruction
+
+//Set the level in the display Port
+#define mBitSet(port_bit, value)    port_bit    =   value
+
+#define LCDSetPort(instruction)     do{mBitSet(D7, (0x80==(instruction&0x80)));\
+                                       mBitSet(D6, (0x40==(instruction&0x40)));\
+                                       mBitSet(D5, (0x20==(instruction&0x20)));\
+                                       mBitSet(D4, (0x10==(instruction&0x10)));}while(0)
+
+/******************************************************************************/
+/*                        Display Ports Definitions                           */
+/******************************************************************************/
+//#define D7
+//#define D6                 ///////////////////////////////////////////
+//#define D5                 //BEFORE USE THIS LIBRARY IS NECESSARY ////
+//#define D4                 //TO MEKE DISPLAY PORT DEFINIES        ////
+//#define EN                 ///////////////////////////////////////////
+//#define RS
+
+/******************************************************************************/
+/*                       Display internal Functions                            */
+/******************************************************************************/
+
+
+
+/******************************************************************************/
+/*                               Display Functions                            */
+/******************************************************************************/
+
+void LCD_Set_Port(unsigned char instruction)
+{
+    mBitSet(D7, (0x80==(instruction&0x80)));
+    mBitSet(D6, (0x40==(instruction&0x40)));
+    mBitSet(D5, (0x20==(instruction&0x20)));
+    mBitSet(D4, (0x10==(instruction&0x10)));
+}
+
+void LCD_Instruction(unsigned char instruction)
+{
+    mBitSet(RS, 0);
+    mBitSet(EN,1);
+   
+    LCDSetPort(instruction);
+    
+    Delay_us(1);
+    mBitSet(EN,0);
+    Delay_us(1);
+    
+    mBitSet(EN,1);
+    
+    instruction <<= 4;
+    LCDSetPort(instruction);
+    
+    Delay_us(1);
+    mBitSet(EN,0);
+    Delay_us(45);
+}
+
+void LCD_Move_Cursor(unsigned char line, unsigned char column)
+{
+    if((1 > column) || (20 < column))         return;
+    switch(line)
+    {
+        case 1:
+            LCD_Instruction(0x80+column-1);
+            break;
+        case 2:
+            LCD_Instruction(0xC0+column-1);
+            break;
+        case 3:
+            LCD_Instruction(0x94+column-1);
+            break;
+        case 4:
+            LCD_Instruction(0xD4+column-1);
+            break;
+        default:
+            break;
+    }
+}
+
+void LCD_Clear_Display(void)
+{
+    LCD_Instruction(CLEAR_DISPLAY);
+    Delay_ms(2);    
+}
+
+void LCD_Return_Home(void)
+{
+    LCD_Instruction(RETURN_HOME);
+    Delay_ms(2);    
+}
+
+void putch(char data)
+{
+    mBitSet(RS, 1);   
+    mBitSet(EN,1);
+    
+    LCDSetPort(data);
+
+    Delay_us(1);
+    mBitSet(EN,0);
+    Delay_us(1);
+
+    mBitSet(EN,1);
+    
+    data <<= 4;
+    LCDSetPort(data);
+    
+    Delay_us(1);
+    mBitSet(EN,0);
+    Delay_us(45);      
+}
+
+void LCD_Write_Char(char data)
+{
+    mBitSet(RS, 1);   
+    mBitSet(EN,1);
+    
+    LCDSetPort(data);
+
+    Delay_us(1);
+    mBitSet(EN,0);
+    Delay_us(1);
+
+    mBitSet(EN,1);
+    
+    data <<= 4;
+    LCDSetPort(data);
+    
+    Delay_us(1);
+    mBitSet(EN,0);
+    Delay_us(45);      
+}
+
+void LCD_Set_New_Simbol(unsigned char *simbol, unsigned char simbol_address)
+{
+    LCD_Instruction(SET_CGRAM_ADDRESS|simbol_address);
+    
+    for(unsigned char i=0; 8>i; i++)
+        putch(*(simbol++));
+    
+    LCD_Instruction(SET_DDRAM_ADDRESS);
+}
+
+void LCD_Set_Latin_Char(void)
+{
+    uint8_t c_cedilha[8] = {0b00000000,
+                            0b00000000,
+                            0b00001110,
+                            0b00010000,
+                            0b00010001,
+                            0b00001110,
+                            0b00000100,
+                            0b00001100}; 
+    LCD_Set_New_Simbol(c_cedilha,0);
+
+    uint8_t a_til[8]    =  {0b00001101,
+                            0b00010010,
+                            0b00000000,
+                            0b00001110,
+                            0b00000001,
+                            0b00001111,
+                            0b00010001,
+                            0b00011111};
+    LCD_Set_New_Simbol(a_til,8);
+    
+    uint8_t a_agudo[8]  =  {0b00000010,
+                            0b00000100,
+                            0b00000000,
+                            0b00001110,
+                            0b00000001,
+                            0b00001111,
+                            0b00010001,
+                            0b00011111};
+    LCD_Set_New_Simbol(a_agudo,16);
+}
+
+void LCD_Initializer()
+{    
+    Delay_ms(40);
+    mBitSet(RS, 0);
+    
+    mBitSet(EN,1);
+    LCDSetPort(DATA_8_BIT_MODE);
+    Delay_us(1);
+    mBitSet(EN,0);
+    Delay_us(1);
+    
+    Delay_ms(5);
+    
+    mBitSet(EN,1);
+    LCDSetPort(DATA_8_BIT_MODE);
+    Delay_us(1);
+    mBitSet(EN,0);
+    Delay_us(1);
+    
+    Delay_us(500);
+    
+    mBitSet(EN,1);
+    LCDSetPort(DATA_8_BIT_MODE);
+    Delay_us(1);
+    mBitSet(EN,0);
+    Delay_us(1); 
+
+    mBitSet(EN,1);    
+    LCDSetPort(DATA_4_BIT_MODE);
+    Delay_us(1);
+    mBitSet(EN,0);
+    Delay_us(1);        
+    
+    LCD_Instruction(DATA_4_BIT_MODE|TWO_LINE_DISPLAY);
+    LCD_Instruction(DISPLAY_ON);
+    LCD_Instruction(CLEAR_DISPLAY);
+    Delay_ms(2);
+    LCD_Instruction(MOVE_FOWARD);
+}
---
+++ lcd_4bit_Mode.h
@@ -0,0 +1,51 @@
+/* Microchip Technology Inc. and its subsidiaries.  You may use this software 
+ * and any derivatives exclusively with Microchip products. 
+ * 
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS".  NO WARRANTIES, WHETHER 
+ * EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
+ * WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A 
+ * PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION 
+ * WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION. 
+ *
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+ * INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+ * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
+ * BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE 
+ * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS 
+ * IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF 
+ * ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ * MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE 
+ * TERMS. 
+ */
+
+/* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef LCD_4BITMODE_H
+    #define	LCD_4BITMODE_H
+    
+    #define C_CEDILHA               0
+    #define A_TIL                   1
+    #define A_AGUDO                 2
+    
+    #include <stdio.h>
+    
+    void LCD_Initializer(void);
+    void LCD_Instruction(unsigned char instruction);
+    void putch(char data);
+    void LCD_Write_Char(char data);
+    void LCD_Move_Cursor(unsigned char line, unsigned char column);
+    void LCD_Set_New_Simbol(unsigned char *simbol, unsigned char simbol_address);
+    void LCD_Set_Latin_Char(void);
+    void LCD_Clear_Display(void);
+    void LCD_Return_Home(void);
+
+#endif	/* LCD_4BITMODE_H */
+
