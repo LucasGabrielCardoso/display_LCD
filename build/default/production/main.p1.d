e56ef61a5c3097d916614fcd86dad115db674583
LucasGabrielCardoso <l.c-gabriel@hotmail.com>
2023-01-19 21:30:59 -0300

    driver display lcd rev0

--- build/default/production/main.p1.d
+++
@@ -1,7 +0,0 @@
-build/default/production/main.p1:  \
-main.c  \
-system.h  \
-system_config.h  \
-device_config.h  \
-lcd_4bit_Mode.h  \
-screen.h 
--- build/default/production/screen.p1.d
+++
@@ -1,4 +0,0 @@
-build/default/production/screen.p1:  \
-screen.c  \
-screen.h  \
-lcd_4bit_Mode.h 
